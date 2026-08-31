9f76d38b4e883e7b20a40d69a75ad13310916ad4
Lizzy <lizzy29perry@gmail.com>
2026-08-31 13:32:22 +0000

    finished compiler and interpreter with testinf yayyyy

--- src/python/compiler.py
+++ src/python/compiler.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from typing import Any
+from typing import Generator
 from enum import auto, StrEnum
+from .parser import BinOp
 
 
 class BytecodeType(StrEnum):
@@ -12,3 +14,27 @@ class BytecodeType(StrEnum):
 class Bytecode:
     type: BytecodeType
     value: Any = None
+
+#will produce bytecode ops
+class Compiler:
+    def __init__(self, tree: BinOp) -> None:
+        self.tree = tree
+
+    #uses treenodes and makes the bytecode ops
+    def compile(self) -> Generator[Bytecode, None, None]:
+        left = self.tree.left
+        yield Bytecode(BytecodeType.PUSH, left.value)
+
+        right = self.tree.right
+        yield Bytecode(BytecodeType.PUSH, right.value)
+
+        yield Bytecode(BytecodeType.BINOP, self.tree.op)
+
+#testiinnggggggg
+if __name__ == "__main__":
+    from .tokenizer import Tokenizer
+    from .parser import Parser
+
+    compiler = Compiler(Parser(list(Tokenizer("3 + 5"))).parse())
+    for bc in compiler.compile():
+        print(bc)
---
+++ src/python/interpreter.py
@@ -0,0 +1,63 @@
+from .compiler import Bytecode, BytecodeType
+
+
+
+#make stack to keep track of our calculations
+class Stack:
+    def __init__(self) -> None:
+        self.stack: list[int] = []
+
+    def push(self, item: int) -> None:
+        self.stack.append(item)
+
+    def pop(self) -> int:
+        return self.stack.pop()
+
+    def peek(self) -> int:
+        return self.stack[-1]
+
+    def __repr__(self) -> str:
+        return f"Stack({self.stack})"
+
+#accepts a list of bytecode operations 
+#and will go through list of bytecodes 
+#to interpret one at a timne
+class Interpreter:
+    def __init__(self, bytecode: list[Bytecode]) -> None:
+        self.stack = Stack()
+        self.bytecode = bytecode
+        self.ptr: int = 0
+
+    #will check which bytecode its looking at and then perform the op it implements
+    def interpret(self) -> None:
+        for bc in self.bytecode:
+            if bc.type == BytecodeType.PUSH:
+                self.stack.push(bc.value)
+            elif bc.type == BytecodeType.BINOP:
+                right = self.stack.pop()
+                left = self.stack.pop()
+                if bc.value == "+":
+                    result = left + right
+                elif bc.value == "-":
+                    result = left - right
+                else:
+                    raise RuntimeError(f"Unknown operator {bc.value}.")
+                self.stack.push(result)
+
+        print("Done!")
+        print(self.stack)
+
+
+#testing again
+if __name__ == "__main__":
+    import sys
+
+    from .tokenizer import Tokenizer
+    from .parser import Parser
+    from .compiler import Compiler
+
+    code = sys.argv[1]
+    tokens = list(Tokenizer(code))
+    tree = Parser(tokens).parse()
+    bytecode = list(Compiler(tree).compile())
+    Interpreter(bytecode).interpret()
